2276401e6fd044368290800c0c17dede74dc3860
yavidor <yonatan@avidor.org>
2025-09-13 01:23:26 +0300

    Annoying offset

--- bmp/main.go
+++ bmp/main.go
@@ -8,6 +8,15 @@ import (
 	"unsafe"
 )
 
+func BytesToInteger[T Integer](bytes []byte, offset int) T {
+	var result T
+	size := min(len(bytes)-offset, int(unsafe.Sizeof(result)))
+	for i := range size {
+		result |= T(bytes[i+offset]) << ((size - 1 - i) * 8)
+	}
+	return result
+}
+
 type FileHeader struct {
 	Signature  uint16
 	Size       uint32
@@ -27,50 +36,39 @@ type Integer interface {
 func (fh *FileHeader) read(data []byte, offset int) int {
 	v := reflect.ValueOf(fh).Elem()
 	for i := range v.NumField() {
-		fmt.Println(offset)
 		field := v.Field(i)
 		switch field.Kind() {
 		case reflect.Int8:
-			field.SetInt(int64(convertRunesToType[int8](data, offset)))
+			field.SetInt(int64(BytesToInteger[int8](data, offset)))
 			offset += 1
 		case reflect.Int16:
-			field.SetInt(int64(convertRunesToType[int16](data, offset)))
+			field.SetInt(int64(BytesToInteger[int16](data, offset)))
 			offset += 2
 		case reflect.Int32:
-			field.SetInt(int64(convertRunesToType[int32](data, offset)))
+			field.SetInt(int64(BytesToInteger[int32](data, offset)))
 			offset += 3
 		case reflect.Int64:
-			field.SetInt(int64(convertRunesToType[int64](data, offset)))
+			field.SetInt(int64(BytesToInteger[int64](data, offset)))
 			offset += 4
 		case reflect.Uint8:
-			field.SetUint(uint64(convertRunesToType[uint8](data, offset)))
+			field.SetUint(uint64(BytesToInteger[uint8](data, offset)))
 			offset += 1
 		case reflect.Uint16:
-			field.SetUint(uint64(convertRunesToType[uint16](data, offset)))
+			field.SetUint(uint64(BytesToInteger[uint16](data, offset)))
 			offset += 2
 		case reflect.Uint32:
-			field.SetUint(uint64(convertRunesToType[uint32](data, offset)))
+			field.SetUint(uint64(BytesToInteger[uint32](data, offset)))
 			offset += 3
 		case reflect.Uint64:
-			field.SetUint(uint64(convertRunesToType[uint64](data, offset)))
+			field.SetUint(uint64(BytesToInteger[uint64](data, offset)))
 			offset += 4
 		}
-		fmt.Println(unsafe.Sizeof(field))
-		fmt.Println(field.Type().Name())
 	}
 	return offset
 }
 
 const FILE_NAME = "./images/greenland_grid_velo.bmp"
 
-func convertRunesToType[T Integer](bytes []byte, offset int) T {
-	var result T
-	size := min(len(bytes)-offset, int(unsafe.Sizeof(result)))
-	for i := range size {
-		result |= T(bytes[i+offset]) << ((size - 1 - i) * 8)
-	}
-	return result
-}
 func main() {
 	data, err := os.ReadFile(FILE_NAME)
 	if err != nil {
